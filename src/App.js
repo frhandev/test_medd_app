6631e8049ad7054d458c6ea51bd5823fc416a7f2
frhandev <frhandev@gmail.com>
2024-12-03 11:47:45 -0500

    InstantConsultationBooking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import LandingPage from "./Components/Landing_Page/LandingPage";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
 
 function App() {
   return (
